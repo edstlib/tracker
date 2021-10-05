0f45ee521e21e56acd880313518eef57470d57b6
tonikustiana <toni.kustiana@sg-dsa.com>
2021-10-05 07:52:45 +0700

    change secret filename output

--- tracker/src/main/java/id/co/edtslib/tracker/di/CoreModule.kt
+++ tracker/src/main/java/id/co/edtslib/tracker/di/CoreModule.kt
@@ -39,7 +39,7 @@ val sharedPreferencesModule = module {
 
             EncryptedSharedPreferences.create(
                 get(),
-                "secret_shared_prefs",
+                "edts_tracker_secret_shared_prefs",
                 masterKey,
                 EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
                 EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
