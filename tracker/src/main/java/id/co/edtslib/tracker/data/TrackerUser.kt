b8f9f3e9f7f288f94cc2623e7fb44c4c8f5930de
tonikustiana <toni.kustiana@sg-dsa.com>
2023-01-28 06:25:26 +0700

    fix tracker

--- tracker/src/main/java/id/co/edtslib/tracker/data/TrackerUser.kt
+++ tracker/src/main/java/id/co/edtslib/tracker/data/TrackerUser.kt
@@ -2,22 +2,18 @@ package id.co.edtslib.tracker.data
 
 import android.annotation.SuppressLint
 import com.google.gson.annotations.SerializedName
-import id.co.edtslib.tracker.util.ConnectivityUtil
 
 data class TrackerUser (
     @SerializedName("session_id")
     val sessionId: String?,
     @SerializedName("user_id")
-    val userId: Long?,
-    @SerializedName("user_ip_address")
-    val userIpAddress: String?
+    val userId: Long?
 ){
     companion object {
         @SuppressLint("HardwareIds")
         fun create(sessionId: String?, userId: Long) : TrackerUser {
             return TrackerUser(
-                sessionId, userId,
-                ConnectivityUtil.getIPAddress(true)
+                sessionId, userId
             )
         }
     }
